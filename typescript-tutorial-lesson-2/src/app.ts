f7364e2da8f81ca7eae9cb205b9c028100c5cdf2
jamieando <jamie_anderson81@hotmail.com>
2021-05-30 08:26:50 +1000

    Added classes folder to better organise code

--- typescript-tutorial-lesson-2/src/app.ts
+++ typescript-tutorial-lesson-2/src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
-	client: string;
-	details: string;
-	amount: number;
-
-	constructor(c: string, d: string, a: number) {
-		this.client = c;
-		this.details = d;
-		this.amount = a;
-	}
-
-	format() {
-		return `${this.client} owes $${this.amount} for ${this.details}`
-	}
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
 const invTwo = new Invoice('Luigi', 'Work on the Mario website', 300);
@@ -22,7 +7,9 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-console.log(invoices);
+invoices.forEach(inv => {
+	console.log(inv.client, inv.amount, inv.format());
+})
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
