8d7eb9ccc3dee2eaa91e3b7923b8c2316a0768e3
jamieando <jamie_anderson81@hotmail.com>
2021-05-30 10:35:34 +1000

    Finished project

--- typescript-tutorial-lesson-2/src/app.ts
+++ typescript-tutorial-lesson-2/src/app.ts
@@ -1,29 +1,7 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-import { HasFormatter } from './interfaces/HasFormatter.js'
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('Yoshi', 'Web work', 250)
-// docTwo = new Payment('Mario', 'Plumbing work', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
-// const invTwo = new Invoice('Luigi', 'Work on the Mario website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach(inv => {
-// 	console.log(inv.client, inv.amount, inv.format());
-// })
+import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -32,6 +10,10 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
 	e.preventDefault();
@@ -44,5 +26,5 @@ form.addEventListener('submit', (e: Event) => {
 		doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
 	}
 
-	console.log(doc);
-})
+	list.render(doc, type.value, 'end');
+});
